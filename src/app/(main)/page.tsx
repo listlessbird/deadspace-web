f71809096ad5461084b95cad7bd9379493d19465
listlessbird <124798751+listlessbird@users.noreply.github.com>
2024-07-23 05:23:49 +0530

    feat: setup social insights

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,12 +1,32 @@
 import { PostEditor } from "@/app/(main)/_components/posts/editor/post-editor"
+import { Post } from "@/app/(main)/_components/posts/post"
+import { TrendingSidebar } from "@/app/(main)/_components/trending-topics-bar"
+import { validateRequest } from "@/auth"
+import { db } from "@/db"
+import { schema } from "@/schema"
+import { getPostsByUser } from "@/schema/db-fns"
+import { sql } from "drizzle-orm"
 import Image from "next/image"
 
-export default function Home() {
+export default async function Home() {
+  const { user } = await validateRequest()
+
+  if (!user) return null
+
+  const posts = await getPostsByUser(user?.id)
+
   return (
-    <main className="w-full">
-      <div className="w-full">
+    <main className="flex w-full min-w-0 gap-5">
+      <div className="w-full min-w-0 space-y-5">
         <PostEditor />
+        {posts.map((post, idx) => (
+          <Post key={post.id} post={post} />
+        ))}
+        {/* <pre className="text-black">
+          <code>{JSON.stringify(posts, null, 2)}</code>
+        </pre> */}
       </div>
+      <TrendingSidebar />
     </main>
   )
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,26 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { formatDate, formatDistanceToNowStrict } from "date-fns"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function getRelativeDate(from: Date) {
+  const current = new Date()
+
+  if (current.getTime() - from.getTime() < 24 * 60 * 60 * 1000) {
+    return formatDistanceToNowStrict(from, { addSuffix: true })
+  } else if (current.getFullYear() === from.getFullYear()) {
+    return formatDate(from, "dd MM")
+  } else {
+    return formatDate(from, "dd MM, yyyy")
+  }
+}
+
+export function formattedNumber(n: number): string {
+  return Intl.NumberFormat("en-US", {
+    notation: "compact",
+    maximumFractionDigits: 1,
+  }).format(n)
 }
