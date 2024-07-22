607de1ba9db663fcbdd84eaeaecffb22a6b08980
listlessbird <124798751+listlessbird@users.noreply.github.com>
2024-07-23 01:31:25 +0530

    feat: add a sidebar

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,5 +1,6 @@
 import { Navbar } from "@/app/(main)/_components/navbar"
 import { SessionProvider } from "@/app/(main)/hooks/useSession"
+import { Menubar } from "@/app/(main)/menubar"
 import { validateRequest } from "@/auth"
 import { redirect } from "next/navigation"
 
@@ -14,7 +15,11 @@ export default async function Layout({ children }: { children: ReactNode }) {
     <SessionProvider value={session}>
       <div className="flex min-h-screen flex-col">
         <Navbar />
-        <div className="mx-auto max-w-7xl p-5">{children}</div>
+        <div className="mx-auto flex w-full max-w-7xl grow gap-5 p-5">
+          <Menubar className="sticky top-[5.25rem] hidden h-fit flex-none space-y-3 rounded-2xl bg-card px-3 py-5 shadow-sm sm:block lg:px-5 xl:w-80" />
+          {children}
+        </div>
+        <Menubar className="sticky bottom-0 flex w-full justify-center gap-5 border-t bg-card p-3 sm:hidden" />
       </div>
     </SessionProvider>
   )
