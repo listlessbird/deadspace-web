b71154c9312b58e9a34d053584ba8188bcff6a75
listlessbird <124798751+listlessbird@users.noreply.github.com>
2024-07-23 07:19:50 +0530

    fix: button alignment

--- src/app/(main)/_components/posts/editor/post-editor.tsx
+++ src/app/(main)/_components/posts/editor/post-editor.tsx
@@ -37,22 +37,22 @@ export function PostEditor() {
   }, [input, editor])
 
   return (
-    <div className="flex flex-col rounded-2xl bg-card p-5 shadow-sm">
+    <div className="flex flex-col gap-5 rounded-2xl bg-card p-5 shadow-sm">
       <div className="flex gap-5">
         <UserAvatar avatarUrl={user.avatarUrl} className="hidden sm:inline" />
         <EditorContent
           editor={editor}
           className="max-h-[20rem] w-full overflow-y-auto rounded-xl bg-background px-5 py-3"
         />
-        <div className="flex justify-end">
-          <Button
-            disabled={!input.trim()}
-            onClick={onSubmitPost}
-            className="min-w-20"
-          >
-            Announce
-          </Button>
-        </div>
+      </div>
+      <div className="flex justify-end">
+        <Button
+          disabled={!input.trim()}
+          onClick={onSubmitPost}
+          className="min-w-20"
+        >
+          Announce
+        </Button>
       </div>
     </div>
   )
