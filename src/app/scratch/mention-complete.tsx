ce8f03f80cff3284f7a9f4582513afa5f35c53a8
listlessbird <124798751+listlessbird@users.noreply.github.com>
2024-08-28 10:25:58 +0530

    feat(scratch): AutocompleteDropdown

--- src/app/scratch/mention-complete.tsx
+++ src/app/scratch/mention-complete.tsx
@@ -1,16 +1,21 @@
 import { cn } from "@/lib/utils"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 export function AutocompleteDropdown<T extends string>({
   suggestions,
   onSelect,
   inputRef,
+  onOpenChange,
 }: {
   suggestions: T[]
   onSelect: (item: T) => void
   inputRef: React.RefObject<HTMLInputElement>
+  onOpenChange: (isOpen: boolean) => void
 }) {
   const [activeIndex, setActiveIndex] = useState(0)
+
+  const dropdownref = useRef<HTMLUListElement | null>(null)
+
   useEffect(() => {
     setActiveIndex(0)
   }, [suggestions])
@@ -33,6 +38,7 @@ export function AutocompleteDropdown<T extends string>({
       if (e.key === "Enter") {
         e.preventDefault()
         onSelect(suggestions[activeIndex])
+        input?.focus()
       }
     }
 
@@ -43,18 +49,38 @@ export function AutocompleteDropdown<T extends string>({
     }
   }, [inputRef, suggestions, activeIndex, onSelect])
 
+  useEffect(() => {
+    const dropdown = dropdownref.current
+
+    function handleClick(e: MouseEvent) {
+      if (dropdown && !dropdown.contains(e.target as Node)) {
+        onOpenChange(false)
+      }
+    }
+
+    document.addEventListener("click", handleClick)
+
+    return () => {
+      document.removeEventListener("click", handleClick)
+    }
+  }, [onOpenChange])
+
   return (
-    <ul className="absolute flex w-fit flex-col gap-2 rounded border bg-background p-1 py-2 shadow-md">
+    <ul
+      ref={dropdownref}
+      className="absolute flex w-fit flex-col gap-2 rounded border bg-background p-1 py-2 shadow-md"
+    >
       {suggestions.map((item) => (
         <li
           key={item}
           data-active={suggestions[activeIndex] === item}
           className={cn(
             'text-white data-[active="true"]:bg-blue-400',
-            "w-full cursor-pointer rounded-md px-4 py-1",
+            "w-full cursor-pointer rounded-md px-4 py-1 hover:bg-blue-400",
           )}
           onClick={() => {
             onSelect(item)
+            inputRef.current?.focus()
           }}
         >
           {item}
