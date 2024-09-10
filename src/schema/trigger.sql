31ae06498c6899c6b960cde56d88d9545fbe115c
listlessbird <124798751+listlessbird@users.noreply.github.com>
2024-09-10 10:24:06 +0530

    fix agent table trigger

--- src/schema/trigger.sql
+++ src/schema/trigger.sql
@@ -2,16 +2,16 @@
 CREATE OR REPLACE FUNCTION copy_user_avatar_to_agent()
     RETURNS TRIGGER AS $$
 BEGIN
-    IF NEW.avatar is NULL THEN
+    IF NEW.avatar_url is NULL THEN
         select avatar_url into NEW.avatar_url
-        FROM users
+        FROM "user"
         WHERE id = NEW.created_by;
     END IF;
     RETURN NEW;
 END;
 $$ LANGUAGE PLPGSQL;
 
-CREATE TRIGGER set_default_agent_avatar
+CREATE OR REPLACE TRIGGER set_default_agent_avatar
 BEFORE INSERT ON agents
 FOR EACH ROW
 EXECUTE FUNCTION copy_user_avatar_to_agent();
