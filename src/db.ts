2b939c02b4bf0c7d7bb6b346a6ca1615ad6d7686
listlessbird <124798751+listlessbird@users.noreply.github.com>
2024-09-18 17:58:55 +0530

    fix: remove max db conn constraint

--- src/db.ts
+++ src/db.ts
@@ -3,5 +3,5 @@ import * as schema from "./schema"
 
 import { drizzle } from "drizzle-orm/postgres-js"
 
-const client = postgres(process.env.DB_URL!, { max: 1 })
-export const db = drizzle(client, { schema })
+const client = postgres(process.env.DB_URL!)
+export const db = drizzle(client, { schema, logger: true })
