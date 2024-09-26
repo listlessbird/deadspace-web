9185a575e88762b910ea6bf03f87450c109398ef
listlessbird <124798751+listlessbird@users.noreply.github.com>
2024-09-26 13:09:27 +0530

    fix: add a empty content check and enforce it on the db schema

--- drizzle/0000_fresh_mother_askani.sql
+++ drizzle/0000_fresh_mother_askani.sql
@@ -215,6 +215,7 @@ END $$;
 CREATE INDEX IF NOT EXISTS "post_search_idx" ON "posts" USING gin (to_tsvector('english', coalesce("content",'')));
 
 
+
 ALTER TABLE posts
 DROP CONSTRAINT IF EXISTS either_user_or_agent_post;
 
@@ -223,6 +224,15 @@ ADD CONSTRAINT either_user_or_agent_post
 CHECK (
   (user_id IS NULL AND agent_id IS NOT NULL) OR 
   (user_id IS NOT NULL AND agent_id IS NULL)
+);
+
+ALTER TABLE posts
+DROP CONSTRAINT IF EXISTS post_content_length_not_empty;
+
+ALTER TABLE posts
+ADD CONSTRAINT post_content_length_not_empty
+CHECK (
+  LENGTH(content) > 0
 );
 
 
@@ -236,6 +246,16 @@ CHECK (
   (user_id IS NOT NULL AND agent_id IS NULL)
 );
 
+ALTER TABLE comments
+DROP CONSTRAINT IF EXISTS comment_content_length_not_empty;
+
+ALTER TABLE comments
+ADD CONSTRAINT comment_content_length_not_empty
+CHECK (
+  LENGTH(content) > 0
+);
+
+
 ALTER TABLE follower_relation
 DROP CONSTRAINT IF EXISTS no_self_follow;
 
@@ -244,40 +264,3 @@ ADD CONSTRAINT no_self_follow
 CHECK (
   follow_from != follow_to
 );
-
-
-
-CREATE OR REPLACE FUNCTION copy_user_avatar_to_agent()
-    RETURNS TRIGGER AS $$
-BEGIN
-    IF NEW.avatar_url is NULL THEN
-        select avatar_url into NEW.avatar_url
-        FROM "user"
-        WHERE id = NEW.created_by;
-    END IF;
-    RETURN NEW;
-END;
-$$ LANGUAGE PLPGSQL;
-
-CREATE OR REPLACE TRIGGER set_default_agent_avatar
-BEFORE INSERT ON agents
-FOR EACH ROW
-EXECUTE FUNCTION copy_user_avatar_to_agent();
-
-
-CREATE OR REPLACE FUNCTION assign_post_type () RETURNS TRIGGER AS $$
-BEGIN
-   IF NEW.user_id IS NOT NULL THEN
-    NEW.type := 'user';
-  ELSIF NEW.agent_id IS NOT NULL THEN
-    NEW.type := 'agent';
-  ELSE
-    RAISE EXCEPTION 'Either user_id or agent_id must be provided';
-  END IF;
-  RETURN NEW;
-    END;
-$$ LANGUAGE PLPGSQL;
-
-CREATE
-OR REPLACE TRIGGER set_post_type BEFORE INSERT ON posts FOR EACH ROW
-EXECUTE FUNCTION assign_post_type ();
